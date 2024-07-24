a2803110c1883b78f0ffcf8738132eeb6660f7d8
MOHSIN KHAN <87376173+Mohsinkkk@users.noreply.github.com>
2024-07-24 23:38:47 +0530

    Add files via upload

---
+++ largestelemnet.java
@@ -0,0 +1,19 @@
+public class largestelemnet {
+    int A = 200;
+    int B = 300;
+    int C = 400;
+    
+
+    if(A<B && B<C){
+        System.out.println("A is Greater than B and C");
+
+    }
+    else if(B<A && A<C){
+        System.out.println("B is Greater than A But B is not greater C");
+    }
+    else{
+        System.out.println("C is Greater than A and B");
+    }   
+    
+}
+
