75e5459b81e6c88504780b302362458afa41a161
MOHSIN KHAN <87376173+Mohsinkkk@users.noreply.github.com>
2024-08-17 19:58:34 +0530

    Create printString.java

---
+++ String/printString.java
@@ -0,0 +1,34 @@
+package String;
+import java.util.*;
+
+
+public class printString {
+    public static void main(String[] args){
+
+        //( Print hello world)
+        // String str = ("Hello Wolrd");
+        // System.out.println(str);
+
+           //(Character Print)
+    //     char str [] = {'a' , 'b' , 'c' , 'd'};
+    //     String str1 = ("abc");
+    //     String str2 = new String("xyz");
+    // Scanner sc = new Scanner(System.in);
+    //     String name;
+    //     name = sc.nextLine();
+    //     System.out.println(name);
+
+
+          //(Find length of any String)
+        //  String fullName = "Mohsin Khan";
+        //  System.out.println(fullName.length());
+
+        //(Concatenantion)
+        String firstName = "Mohsin";
+        String lastName = "Khan";
+        String fullName = firstName +" "+ lastName;
+        System.out.println(fullName);
+    }
+} 
+ 
+
